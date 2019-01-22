153a53135d8ab7b99d552ab50b6c82c0984eb72d
trineon89 <trineon89@gmail.com>
2019-01-22 15:36:54 +0100

    yolo

--- KonterbontLODConnector/Main.cs
+++ KonterbontLODConnector/Main.cs
@@ -15,37 +15,55 @@ namespace WindowsFormsApp1
 {
     public partial class Main : Form
     {
+        String theResponse = null;
+
         public Main()
         {
             InitializeComponent();
         }
 
-        private async Task<HttpResponseMessage> FetchWords(string XML)
+        private async Task FetchWords(string XML)
         {
             var httpClient = new HttpClient();
             var httpContent = new HttpRequestMessage
-
-
-           {
-               RequestUri = new Uri("https://www.lod.lu/php/getartde.php?artid="+XML+".xml"),
-               Method = HttpMethod.Get,
-               Headers =
+            {
+                RequestUri = new Uri("https://www.lod.lu/php/getartde.php?artid=" + XML + ".xml"),
+                Method = HttpMethod.Get,
+                Headers =
                            {
-                              // { HttpRequestHeader.ContentType.ToString(), "text/xml; charset=\"utf-8\";" },
                               { HttpRequestHeader.Host.ToString(), "www.lod.lu" },
                                { HttpRequestHeader.Referer.ToString(), "https://www.lod.lu/" }
                            }
-           };
+            };
 
-            //HttpResponseMessage _response =  await httpClient.SendAsync(httpContent);
-            httpClient.Timeout = TimeSpan.FromMilliseconds(250);
+            httpClient.Timeout = TimeSpan.FromSeconds(30);
             var _response = await httpClient.SendAsync(httpContent);
-            return _response;
+            _response.EnsureSuccessStatusCode();
+            string responseBody = await _response.Content.ReadAsStringAsync();
+
+            SetResultText(responseBody);
         }
 
-        private void btnFetch_Click(object sender, EventArgs e)
+        private void btnFetch_ClickAsync(object sender, EventArgs e)
         {
-            rtbResult.Text = FetchWords(edtXML.Text).Result.ToString();
+
+            Task.WaitAll(Task.Run(async () => await FetchWords(edtXML.Text)));
+            rtbResult.Text = theResponse;
+        }
+
+
+        public bool ControlInvokeRequired(Control c, Action a)
+        {
+            if (c.InvokeRequired) c.Invoke(new MethodInvoker(delegate { a(); }));
+            else return false;
+
+            return true;
+        }
+
+        private void SetResultText(String txt)
+        {
+            //if (ControlInvokeRequired(rtbResult, () => SetResultText(txt))) return;
+            theResponse = txt;
         }
     }
 }
