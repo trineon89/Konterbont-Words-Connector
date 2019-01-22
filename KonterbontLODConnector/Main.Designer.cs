76136ceeeda645217d8d33d310b134b65312abca
Yannick <infomedia1@autisme.lu>
2019-01-22 10:29:58 +0100

    init v2

---
+++ KonterbontLODConnector/Main.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edtXML = new System.Windows.Forms.TextBox();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // edtXML
+            // 
+            this.edtXML.Location = new System.Drawing.Point(44, 28);
+            this.edtXML.Name = "edtXML";
+            this.edtXML.Size = new System.Drawing.Size(684, 20);
+            this.edtXML.TabIndex = 0;
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.BackColor = System.Drawing.SystemColors.Window;
+            this.rtbResult.Location = new System.Drawing.Point(44, 73);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(780, 576);
+            this.rtbResult.TabIndex = 1;
+            this.rtbResult.Text = "";
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(749, 28);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(75, 23);
+            this.btnFetch.TabIndex = 2;
+            this.btnFetch.Text = "&Fetch";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1401, 791);
+            this.Controls.Add(this.btnFetch);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.edtXML);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox edtXML;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Button btnFetch;
+    }
+}
+
---
+++ KonterbontLODConnector/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+
+namespace WindowsFormsApp1
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var httpContent = new HttpRequestMessage
+            {
+                RequestUri = new Uri("https:///sony/IRCC"),
+                Method = HttpMethod.Get,
+                Headers =
+                        {
+                            { HttpRequestHeader.ContentType.ToString(), "text/xml; charset=\"utf-8\";" },
+                            { "Referer", "1337" }
+                        },
+                Content = new StringContent("<?xml version=\"1.0\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:X_SendIRCC xmlns:u=\"urn:schemas-sony-com:service:IRCC:1\"><IRCCCode>\"" + actionarr[3] + "\"</IRCCCode></u:X_SendIRCC></s:Body></s:Envelope> ", Encoding.UTF8, "text/xml")
+            };
+
+            HttpResponseMessage _response = await httpClient.SendAsync(httpContent);
+        }
+    }
+}
